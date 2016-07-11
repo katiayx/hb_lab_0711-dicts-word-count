408265ad44a5ca323d15535559cbac9b16baccb7
Hackbright Student <info@hackbrightacademy.com>
2016-07-11 13:00:54 -0700

    Completed exercise with correct output

--- wordcount.py
+++ wordcount.py
@@ -1,1 +1,14 @@
 # put your code here.
+document = open("test.txt")
+word_count = {}
+
+for line in document: 
+    line = line.rstrip()
+    line = line.split(" ")
+
+    for word in line:
+        current_count = word_count.get(word,0)
+        word_count[word] = current_count + 1
+
+for word, current_count in word_count.items():
+    print word, current_count
